a78af3718919c038ca1a0ab9fb8e8f59326a45b4
Martin Bernard <119161281+martin-bernard26@users.noreply.github.com>
2025-03-16 18:25:17 +0700

    Update VBA.py

--- VBA.py
+++ VBA.py
@@ -381,8 +381,12 @@ if st.sidebar.button("Pelajaran pertama"):
         with st.expander("Lihat Video kesebelas"):
             st.video("https://res.cloudinary.com/ikip-siliwangi/video/upload/v1740997523/Recording_43_b2mmv6.mp4")
         st.markdown('''
+                    <div style="font-family:'snap itc'; font-size:16px; color:green">Menyimpan dan Membuka Workbook</div>
+                    ''',unsafe_allow_html=True)
+        st.markdown('''
             <div class="pengantar" style="background-color:cyan;color:black">Menyimpan dan Membuka Workbook<br>
-            🔹  Contoh: Menjalankan Sub secara Otomatis dalam 5 Detik</div>''',unsafe_allow_html=True)
+            🔹  Gunakan save type Excel Macro-Enabled Workbook <br>
+            🔹  Projek yang dibuat tidak hilang ketika sudah di close </div>''',unsafe_allow_html=True)
         st.write("Contoh Video Kedua belas (Menyimpan dan Membuka workbook)")
         with st.expander("Lihat Video kedua belas (Menyimpan dan Membuka Workbook)"):
             st.video("https://res.cloudinary.com/ikip-siliwangi/video/upload/v1742123425/Recording_50_cz9o3u.mp4")
