84812773ea061be4d48de32358cdd3995231b535
Martin Bernard <119161281+martin-bernard26@users.noreply.github.com>
2025-10-07 14:37:53 +0700

    Update VBA.py

--- VBA.py
+++ VBA.py
@@ -1318,7 +1318,7 @@ End Sub
         }}
     </style>
 </head>
-<body>
+<body style="background-color:cyan">
     <div id="judul"> ANGKET EVALUASI PEMBELAJARAN VBA FOR EXCEL DALAM PENDIDIKAN MATEMATIKA</div>
     <div>
         <div>
@@ -2126,6 +2126,7 @@ if st.session_state['genai']:
         # Buat prompt dan kirim ke model
         response = model.generate_content(st.session_state.isian)
         st.write(response.text)
+
 
 
 
